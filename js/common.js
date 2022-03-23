7a9cd422b0b1a281b47c08d3348bcc9d2db90440
publisher4m <whm1001@4-m.co.kr>
2022-03-23 14:41:06 +0900

    confirm03

--- js/common.js
+++ js/common.js
@@ -72,6 +72,16 @@ var commonEvent = {
         });
     },
     headerEvent:function(){
+        
+        $(window).on('scroll',function(){
+            var st = $(window).scrollTop();
+            if (st>=100){
+                $('.header').addClass('fixed');
+            }else{
+                $('.header').remClass('fixed');
+            }
+        });
+
         $(document).on('click', '.lang_choice li', function(){
             $('.lang_choice li').removeClass('on');
             $(this).addClass('on');  
@@ -81,14 +91,13 @@ var commonEvent = {
 var onmEvent = {
     init:function(){
         this.onmSwiper();
-        this.onmScroll();
     },
     onmSwiper:function(){
         var Tabs = $('.business_contents .section1 .nav_btn li');
         var Tabs_depth2 = $(".swiper_2depth_tabs p");
-    
+        
         $(".swiper").each(function(index, element){
-            var swiper1_01 = new Swiper('.swiper1_01', {
+            var swiper1 = new Swiper('.swiper1_0' + index, {
                 observer: true,
                 observeParents: true,
                 slidesPerView : 2,
@@ -96,27 +105,16 @@ var onmEvent = {
                 speed: 700,
                 
                 navigation: {
-                    nextEl: '.swiper-button-next01_01',
-                    prevEl: '.swiper-button-prev01_01',
+                    nextEl: '.swiper-button-next01_0' + index,
+                    prevEl: '.swiper-button-prev01_0' + index,
                 },
                 watchOverflow: true,   
             });
-            var swiper1_02 = new Swiper('.swiper1_02', {
-                observer: true,
-                observeParents: true,
-                slidesPerView : 2,
-                spaceBetween: 70,
-                speed: 700,
-                
-                navigation: {
-                    nextEl: '.swiper-button-next01_02',
-                    prevEl: '.swiper-button-prev01_02',
-                },
-                watchOverflow: true
-            });
+
             var $this = $(this);
-            $this.addClass('instance-' + index);
-            var swiper2 = new Swiper('.swiper2', {
+            $this.addClass('swiper' + index);
+
+            var swiper = new Swiper('.swiper' + index, {
                 observer: true,
                 observeParents: true,
                 slidesPerView : 2,
@@ -124,60 +122,8 @@ var onmEvent = {
                 speed: 700,
                 
                 navigation: {
-                    nextEl: '.swiper-button-next02',
-                    prevEl: '.swiper-button-prev02',
-                },
-                watchOverflow: true
-            });
-            var swiper3 = new Swiper('.swiper3', {
-                observer: true,
-                observeParents: true,
-                slidesPerView : 2,
-                spaceBetween: 70,
-                speed: 700,
-                
-                navigation: {
-                    nextEl: '.swiper-button-next03',
-                    prevEl: '.swiper-button-prev03',
-                },
-                watchOverflow: true
-            });
-            var swiper4 = new Swiper('.swiper4', {
-                observer: true,
-                observeParents: true,
-                slidesPerView : 2,
-                spaceBetween: 70,
-                speed: 700,
-                
-                navigation: {
-                    nextEl: '.swiper-button-next04',
-                    prevEl: '.swiper-button-prev04',
-                },
-                watchOverflow: true
-            });
-            var swiper5 = new Swiper('.swiper5', {
-                observer: true,
-                observeParents: true,
-                slidesPerView : 2,
-                spaceBetween: 70,
-                speed: 700,
-                
-                navigation: {
-                    nextEl: '.swiper-button-next05',
-                    prevEl: '.swiper-button-prev05',
-                },
-                watchOverflow: true
-            });
-            var swiper6 = new Swiper('.swiper6', {
-                observer: true,
-                observeParents: true,
-                slidesPerView : 2,
-                spaceBetween: 70,
-                speed: 700,
-                
-                navigation: {
-                    nextEl: '.swiper-button-next06',
-                    prevEl: '.swiper-button-prev06',
+                    nextEl: '.swiper-button-next0' + index,
+                    prevEl: '.swiper-button-prev0' + index,
                 },
                 watchOverflow: true
             });
@@ -188,54 +134,51 @@ var onmEvent = {
                 
                 var Tabs_idx = Tabs.index(this)+1;
                 $('.section2 .nav_box').removeClass('on');
-                $('.section2 .nav_box .box_text, .section2 .nav_box .swiper, .swiper_2depth_tabs').removeClass('aos-animate');
+                $('.box_text, .swiper, .swiper_2depth_tabs, .onm_box_slider_button').removeClass('aos-animate');
                 $('.section2 .box0' + Tabs_idx).addClass('on');
-     
+
+                
+
                 setTimeout(function() {
-                    $('.section2 .box0' + Tabs_idx + ' .box_text, .section2 .box0' + Tabs_idx + ' .swiper, .swiper_2depth_tabs').addClass('aos-animate');
+                    $('.box0' + Tabs_idx + ' .box_text, .box0' + Tabs_idx + ' .swiper, .swiper_2depth_tabs, .box0' + Tabs_idx + ' .onm_box_slider_button').addClass('aos-animate');
+                    $('.swiper1_box .swiper').removeClass('aos-animate');
+                    Tabs_depth2.removeClass('on');
+                    Tabs_depth2.eq(0).addClass('on');
+
+                    $('.swiper1_box').hide();
+                    $('.swiper1_box01').css('display', 'block');
+                    $('.swiper1_box01 .swiper').addClass('aos-animate');
+
+                    swiper1.slideTo(0, 0);
                 }, 0);
+
                 $('.section3 .onm_list').removeClass('on');
                 $('.section3 .onm_list0' + Tabs_idx).addClass('on');
+
                 swiper.slideTo(0, 0);
+                
             });
+            
             Tabs_depth2.on('click', function(){
                 var idx = Tabs_depth2.index(this)+1;
                 var tab_slide = $(this).parent().siblings('.swiper1_box');
+                
                 Tabs_depth2.removeClass('on');
                 $(this).addClass('on');
-                tab_slide.css({display:'none'});
+                
+                tab_slide.hide();
                 $('.swiper1_box0' + idx).css({display:'block'});
-
                 $('.swiper1_box .swiper').removeClass('aos-animate');
+
                 setTimeout(function() {
                     $('.swiper1_box0' + idx + ' .swiper').addClass('aos-animate');
+                    swiper1.slideTo(0, 0);
                 }, 0);
-                swiper1_01.slideTo(0, 0);
-                swiper1_02.slideTo(0, 0);
+                
             });
         });
     },
-    onmScroll:function(){
-        $(window).on('scroll',function(){
-            
-            // var tit = $('.title'),
-            //     tit_line = $('.business_contents .section1 .tit_line');
-            // var st = $(window).scrollTop(),
-            //     winH = top.window.innerHeight,
-                
-                
-            // if ($(tit_line).offset().top < (st+winH*0.9)){  
-            //     tit_line.addClass('on');
-            // }
-            // if ($(tit).hasClass('aos-animate')){  
-            //     setTimeout(function() {
-                    
-            //     }, 500);
-            // } 
-            
-        });
-             
-    },
+
     
 };
 
