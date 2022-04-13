60053d326ef90cba0347989f120200412ce6727b
WooHaem <whm1001@4-m.co.kr>
2022-04-13 18:32:26 +0900

    20220413

--- js/common.js
+++ js/common.js
@@ -589,17 +589,57 @@ var mainEvent = {
     },
 
     parallax: function() {
-        var parallax = document.querySelectorAll(".parallax"),
-            speed = 0.5;
-    
-        window.onscroll = function() {
-            [].slice.call(parallax).forEach(function(el, i) {
-                var windowYOffset = window.pageYOffset,
-                    elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
-    
-                el.style.backgroundPosition = elBackgrounPos;
+        // makes the parallax elements
+        function parallaxIt() {
+            // create variables
+            var $fwindow = $(window);
+            var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+            var $contents = [];
+            var $backgrounds = [];
+        
+            // for each of content parallax element
+            $('[data-type="content"]').each(function(index, e) {
+            var $contentObj = $(this);
+        
+            $contentObj.__speed = ($contentObj.data('speed') || 1);
+            $contentObj.__fgOffset = $contentObj.offset().top;
+            $contents.push($contentObj);
             });
+        
+            // for each of background parallax element
+            $('[data-type="background"]').each(function() {
+            var $backgroundObj = $(this);
+        
+            $backgroundObj.__speed = ($backgroundObj.data('speed') || 1);
+            $backgroundObj.__fgOffset = $backgroundObj.offset().top;
+            $backgrounds.push($backgroundObj);
+            });
+        
+            // update positions
+            $fwindow.on('scroll resize', function() {
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        
+            $contents.forEach(function($contentObj) {
+                var yPos = $contentObj.__fgOffset - scrollTop / $contentObj.__speed;
+        
+                $contentObj.css('top', yPos);
+            })
+        
+            $backgrounds.forEach(function($backgroundObj) {
+                var yPos = -((scrollTop - $backgroundObj.__fgOffset) / $backgroundObj.__speed);
+        
+                $backgroundObj.css({
+                "transform": "translate(0," + yPos + "px)"
+                });
+            });
+            });
+        
+            // triggers winodw scroll for refresh
+            $fwindow.trigger('scroll');
         };
+        
+        parallaxIt();
 
     },
     
